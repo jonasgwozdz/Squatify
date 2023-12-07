70a012f4ab49f996574f5dc92c68b91daf77645c
Linus Herterich <linus.herterich@gmail.com>
2023-12-07 15:22:06 +0100

    new tracking data (unlabeled)

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 # main.py
-
 import os
+import csv
 from multiprocessing import Pool
 from video_processing import process_video
+from tqdm import tqdm
+
+def rename_video(video_file, video_dir, new_name):
+    # Rename the video file
+    new_name_with_ext = new_name + '.mp4'
+    os.rename(os.path.join(video_dir, video_file), os.path.join(video_dir, new_name_with_ext))
+
+    # Return the old and new names
+    return video_file, new_name_with_ext
 
 if __name__ == '__main__':
     video_dir = 'Videos'
-    video_files = [f for f in os.listdir(video_dir) if f.endswith('.mp4')]
+    video_files = [f for f in os.listdir(video_dir) if f.endswith('.mov') or f.endswith('.MOV') or f.endswith('.mp4')]
 
     with Pool() as p:
-        p.starmap(process_video, [(video_file, video_dir) for video_file in video_files])
+        # Rename the video files
+        results = list(tqdm(p.starmap(rename_video, [(video_file, video_dir, f'video{index:02}') for index, video_file in enumerate(video_files, start=1)]), total=len(video_files)))
+
+        # Save the old and new names of the video files in a CSV file in the video directory
+        with open(os.path.join(video_dir, 'videofilenames.csv'), 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['Old Name', 'New Name'])
+            writer.writerows(results)
+
+        # Process the video files
+        tqdm(p.starmap(process_video, [(result[1], video_dir) for result in results]), total=len(results))
--- video_processing.py
+++ video_processing.py
@@ -1,10 +1,13 @@
 # video_processing.py
-
+import logging
 import os
 import cv2
 import mediapipe as mp
-import numpy as np
 import pandas as pd
+
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
@@ -13,6 +16,7 @@ mp_pose = mp.solutions.pose
 body_parts = ['Nose', 'Left eye inner', 'Left eye', 'Left eye outer', 'Right eye inner', 'Right eye', 'Right eye outer', 'Left ear', 'Right ear', 'Mouth left', 'Mouth right', 'Left shoulder', 'Right shoulder', 'Left elbow', 'Right elbow', 'Left wrist', 'Right wrist', 'Left pinky', 'Right pinky', 'Left index', 'Right index', 'Left thumb', 'Right thumb', 'Left hip', 'Right hip', 'Left knee', 'Right knee', 'Left ankle', 'Right ankle', 'Left heel', 'Right heel', 'Left foot index', 'Right foot index']
 
 def process_video(video_file, video_dir):
+    logging.info(f'Processing video: {video_file}')
     video_path = os.path.join(video_dir, video_file)
     cap = cv2.VideoCapture(video_path)
 
@@ -52,4 +56,6 @@ def process_video(video_file, video_dir):
     tracking_data_dir = 'tracking_data'
     if not os.path.exists(tracking_data_dir):
         os.makedirs(tracking_data_dir)
-    pose_df.to_csv(os.path.join(tracking_data_dir, f'Tracking_{video_file[:-4]}.csv'), index=False)
+    csv_file = os.path.join(tracking_data_dir, f'Tracking_{video_file[:-4]}.csv')
+    pose_df.to_csv(csv_file, index=False)
+    logging.info(f'Tracking data for {video_file} stored in {csv_file}')
